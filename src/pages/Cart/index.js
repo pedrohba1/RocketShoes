729610d4fe005955df9a9de1305f9451a91294a3
Pedro <pedrohba18@gmail.com>
2020-03-06 23:14:26 -0300

    componente de carrinho vazio

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,45 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import { Container, ShoppingList } from './styles';
 
 import CartItem from '../../components/CartItem';
 import CartBottom from '../../components/CartBottom';
+import EmptyCart from '../../components/EmptyCart';
 
-export default class Cart extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            products: [
-                {
-                    id: 3,
-                    title: 'Tênis Adidas Duramo Lite 2.0',
-                    price: 219.9,
-                    image:
-                        'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
-                    formattedPrice: 'R$ 219,9',
-                },
-                {
-                    id: 4,
-                    title: 'Tênis Adidas Duramo Lite 2.0',
-                    price: 219.9,
-                    image:
-                        'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
-                    formattedPrice: 'R$ 219,9',
-                },
-            ],
-        };
-    }
-
-    render() {
-        const { products } = this.state;
-        return (
-            <Container>
-                <ShoppingList
-                    data={products}
-                    renderItem={product => <CartItem product={product.item} />}
-                    keyExtractor={product => String(product.id)}
-                />
-                <CartBottom />
-            </Container>
-        );
-    }
+function Cart({ cart, cartSize }) {
+    return (
+        <Container>
+            <ShoppingList
+                data={cart}
+                renderItem={product => <CartItem product={product.item} />}
+                keyExtractor={product => String(product.id)}
+                ListEmptyComponent={<EmptyCart />}
+            />
+            {cartSize > 0 ? <CartBottom /> : null}
+        </Container>
+    );
 }
+
+const mapStateToProps = state => ({
+    cart: state.cart,
+    cartSize: state.cart.length,
+});
+
+export default connect(mapStateToProps)(Cart);
